8d01b7005dce820a097b9df21a07bd1631e69ba4
swexyio <joergonzalez.jg@gmail.com>
2026-07-28 03:03:24 -0500

    inbattlepyramid

--- src/start_menu.c
+++ src/start_menu.c
@@ -624,7 +624,7 @@ static bool32 InitStartMenuStep(void)
     case 3:
         if (GetSafariZoneFlag())
             ShowSafariBallsWindow();
-        else if (InBattlePyramid())
+        else if (InBattlePyramid_())
             ShowPyramidFloorWindow();
         sInitStartMenuData[0]++;
         break;
@@ -799,7 +799,7 @@ static bool8 StartMenuPokemonCallback(void)
         return TRUE;
     }
 
-    if (!GetSafariZoneFlag() && !InBattlePyramid() && gSaveBlock2Ptr->playTimeSeconds == 0) 
+    if (!GetSafariZoneFlag() && !InBattlePyramid_() && gSaveBlock2Ptr->playTimeSeconds == 0) 
     {
         RemoveExtraStartMenuWindows();
         ShowTimeWindow();
